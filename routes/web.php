4cd33078d6dfb0477ce90d6aeed106f3125101f0
HenrySserwadda <sserwaddah12@gmail.com>
2025-06-17 13:38:35 +0300

    added logout function

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Http\Controllers\PagesController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/about', function(){
+    return view('about');
 });
 
 Route::get('/dashboard', function () {
