43485910f066183b61849505c097d925f6361fe3
HenrySserwadda <sserwaddah12@gmail.com>
2025-06-22 21:40:23 +0300

    s

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('category');
-            $table->enum('status',['pending','approved','rejected']->default('pending'));
-            $table->boolean('is_admin'->default(0));
+            $table->enum('status', ['pending', 'approved', 'rejected'])->default('pending');
+            $table->boolean('is_admin')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
