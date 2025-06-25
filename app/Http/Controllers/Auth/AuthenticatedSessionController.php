f1f3c3d8d4dacb66280383b3898c1e6c17dfd57d
HenrySserwadda <sserwaddah12@gmail.com>
2025-06-25 21:04:04 +0300

    changes

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,8 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        $user = Auth::user();
+        return redirect($user->redirectToDashboard());
     }
 
     /**
