ab56e3a8710a19301ab4fb7b5540493c70cf016e
JAMEY755 <jamevictor755@gmail.com>
2025-07-07 04:19:59 +0300

    Java Server Update

--- src/main/java/com/vendor/validator/Model/Vendor.java
+++ src/main/java/com/vendor/validator/Model/Vendor.java
@@ -2,30 +2,38 @@ package com.vendor.validator.Model;
 
 public class Vendor {
 
-    public String name;
+    public String  name;
+    public String CEO;
+    public String email;
+    public int phone;
+
     public int capital;
     public int revenue;
     public int debt;
     public int experience;
-    public boolean blacklisted;
+
+    public boolean industrystandards;
     public boolean license;
     public boolean taxCertificate;
 
-    public Vendor(String name, int capital, int revenue, int debt, int experience,
-                  boolean blacklisted, boolean license, boolean taxCertificate) {
+    public Vendor(String name, String CEO, String email,int phone, int capital, int revenue, int debt, int experience,
+                  boolean industrystandards, boolean license, boolean taxCertificate) {
         this.name = name;
+        this.CEO = CEO;
+        this.email = email;
+        this.phone = phone;
         this.capital = capital;
         this.revenue = revenue;
         this.debt = debt;
         this.experience = experience;
-        this.blacklisted = blacklisted;
+        this.industrystandards = industrystandards;
         this.license = license;
         this.taxCertificate = taxCertificate;
     }
 
     @Override
     public String toString() {
-        return String.format("Vendor: %s\nCapital: %d\nRevenue: %d\nDebt: %d\nExperience: %d\nBlacklisted: %b\nLicense: %b\nTaxCertificate: %b\n",
-                name, capital, revenue, debt, experience, blacklisted, license, taxCertificate);
+        return String.format("Company Name: %s\nCEO: %s\nEmail: %s\nContact: %d\nCapital: %d\nRevenue: %d\nDebt: %d\nExperience: %d\nIndustry Standards: %b\nLicense: %b\nTaxCertificate: %b\n",
+                name, CEO, email, phone, capital, revenue, debt, experience, industrystandards, license, taxCertificate);
     }
 }
