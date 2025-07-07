3a065004f635db7a18a51de3d0e8513a82a895fa
JAMEY755 <jamevictor755@gmail.com>
2025-07-07 15:43:51 +0300

    Migrations Update

--- database/migrations/2024_01_01_000001_create_orders_table.php
+++ database/migrations/2024_01_01_000001_create_orders_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->string('user_id');
-            $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');
+             $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');
             $table->string('location');
             $table->string('mobile');
             $table->decimal('total', 10, 2);
